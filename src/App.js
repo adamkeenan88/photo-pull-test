dbe0a46e2aba27f6aa6de4a1f6431ad79929e75a
admin <admin@admins-MacBook-Pro.local>
2022-05-04 17:35:33 -0700

    test project setup with API call and display info with CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [showMore, setShowMore] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => {
+        setPhotos(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  function handleClick() {
+    setShowMore(!showMore);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        {photos
+          ? photos.map(
+              (photo, index) =>
+                index > 0 &&
+                index <= 10 && (
+                  <div className="photoInfo" key={index}>
+                    <img src={photo.thumbnailUrl} alt="thumbnail" />
+                    <p>{photo.title}</p>
+                  </div>
+                )
+            )
+          : "Waiting on Photos"}
+        <button onClick={handleClick}>Load More</button>
+        <div>
+          {showMore === true
+            ? photos.map(
+                (photo, index) =>
+                  index > 10 &&
+                  index <= 20 && (
+                    <div className="photoInfo" key={index}>
+                      <img src={photo.thumbnailUrl} alt="thumbnail" />
+                      <p>{photo.title}</p>
+                    </div>
+                  )
+              )
+            : null}
+        </div>
+      </>
     </div>
   );
 }
